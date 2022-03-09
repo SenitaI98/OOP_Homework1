567bf92e2753883cfb7be258168854d8694ad901
SenitaI98 <senita.isovic@live.com>
2022-03-09 19:55:45 +0100

    Updated

--- src/qamp/Main.java
+++ src/qamp/Main.java
@@ -6,39 +6,38 @@ public class Main {
     public static void main(String[] args) {
         Calculator myCalculator = new Calculator();
         Scanner reader = new Scanner(System.in);
-        System.out.println("Please choose the action you want(1,2,3,4,5,6,7): \n1.Sum of 2 numbers \n2.Difference of 2 numbers \n3.Product of 2 numbers \n4.Quotient of 2 numbers \n5.Area of a circle \n6.Sum of N elements \n7.Product of N elements");
+        System.out.println("Please choose the action you want (1,2,3,4,5,6,7): \n1.Sum of 2 numbers \n2.Difference of 2 numbers \n3.Product of 2 numbers \n4.Quotient of 2 numbers \n5.Area of a circle \n6.Sum of N elements \n7.Product of N elements");
         int action = reader.nextInt();
+        if (action == 1 || action == 2 || action == 3 || action == 4) {
+            myCalculator.setNumberA(numberA());
+            myCalculator.setNumberB(numberB());
+        } else if (action == 5) {
+            myCalculator.setNumberA(radius());
+        } else if (action == 6 || action == 7) {
+            myCalculator.setMyArray(array());
+        } else {
+            System.out.println("You choose wrong action!");
+        }
         switch (action) {
             case 1:
-                myCalculator.setNumberA(numberA());
-                myCalculator.setNumberB(numberB());
                 System.out.println("Sum of numbers " + myCalculator.numberA + " and " + myCalculator.numberB + " is " + myCalculator.sum());
                 break;
             case 2:
-                myCalculator.setNumberA(numberA());
-                myCalculator.setNumberB(numberB());
                 System.out.println("Difference of numbers " + myCalculator.numberA + " and " + myCalculator.numberB + " is " + myCalculator.diff());
                 break;
             case 3:
-                myCalculator.setNumberA(numberA());
-                myCalculator.setNumberB(numberB());
                 System.out.println("Product of numbers " + myCalculator.numberA + " and " + myCalculator.numberB + " is " + myCalculator.product());
                 break;
             case 4:
-                myCalculator.setNumberA(numberA());
-                myCalculator.setNumberB(numberB());
                 System.out.println("Quotient of numbers " + myCalculator.numberA + " and " + myCalculator.numberB + " is " + myCalculator.quotient());
                 break;
             case 5:
-                myCalculator.setNumberA(radius());
-                System.out.println("Area of a circle with radius " + myCalculator.numberA + " is " + myCalculator.areaWithRadiusNumberA());
+                System.out.println("Area is: " + myCalculator.areaWithRadiusNumberA());
                 break;
             case 6:
-                myCalculator.setMyArray(array());
                 System.out.println("Sum of elements in array is " + myCalculator.sumElementInArray(myCalculator.myArray));
                 break;
             case 7:
-                myCalculator.setMyArray(array());
                 System.out.println("Product of elements in array is " + myCalculator.productElementInArray(myCalculator.myArray));
                 break;
         }
